adc6a850fc09631ff99fc1de141c92c828ef108a
BadbCatha <inia.em@gmail.com>
2017-12-07 13:40:26 +0100

    add edit option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
 
 	def create
 		#render plain: params[:task].inspect
-		@task=Task.new(params.require(:task).permit(:to_do,:description))
+		@task=Task.new(task_params)
 		@task.save
 		redirect_to @task
 	end
@@ -17,6 +17,26 @@ class TasksController < ApplicationController
 		@tasks=Task.all.order(created_at: :desc)
 
 	end
+
+	def edit
+		@task=Task.find(params[:id])
+	end
+
+	def update
+		@task=Task.find(params[:id])
+
+		if @task.update(task_params)
+			redirect_to @task
+		else 
+			render'edit'
+		end 
+	end
+
+	private
+
+  		def task_params 
+  			params.require(:task).permit(:to_do,:description)
+		end
 end
 
 
